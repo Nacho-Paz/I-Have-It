91b4ccf3451e2b905a47df80ef6028a5119d5193
Nacho <ignaciojavierpazbffs@gmail.com>
2021-11-23 18:30:53 -0300

    Upload IHaveIt.java

---
+++ IHaveIt/src/main/java/com/thecodecorner/IHaveIt/IHaveItApplication.java
@@ -0,0 +1,13 @@
+package com.thecodecorner.IHaveIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHaveItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IHaveItApplication.class, args);
+	}
+
+}
---
+++ IHaveIt/src/test/java/com/thecodecorner/IHaveIt/IHaveItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecodecorner.IHaveIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IHaveItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
